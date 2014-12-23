c1f747f047492ed746002a9a239233ab26f30b04
gtt116 <gtt116@gmail.com>
2014-12-23 10:26:24 +0800

    update run script

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-proxypypi2  -P proxypypi2.pid -l proxypypi2.log -o proxypypi2.console -d mirrors/  -p  8081 $1
+#!/bin/bash
+
+MIRROR_DIR=/home/data/mirrors/
+INDEX_URL=http://pypi.douban.com/simple
+
+proxypypi2  -P proxypypi2.pid -l proxypypi2.log -o proxypypi2.console \
+    -d $MIRROR_DIR \
+    -i $INDEX_URL\
+    -p 8081 $1
